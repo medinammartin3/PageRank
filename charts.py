6244cb33bafd9a746e0521c794bb731e951e2308
medinammartin3 <medinammartin3@gmail.com>
2026-01-13 22:57:52 -0500

    Add plot charts

--- charts.py
+++ charts.py
@@ -325,12 +325,13 @@ def chart9():
 
 ## GÉNÉRATION DES GRAPHIQUES ##
 
-# chart1()
-# chart2()
-# chart3()
-# chart4()
-# chart5()
-# chart6()
-# chart7()
-# chart8()
-# chart9()
+if __name__ == "__main__":
+    chart1()
+    chart2()
+    chart3()
+    chart4()
+    chart5()
+    chart6()
+    chart7()
+    chart8()
+    chart9()
